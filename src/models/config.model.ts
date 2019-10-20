eb4769d8739c2faaa86ce29bb578796c77e78116
kasperlauge <kasperlaugemadsen@gmail.com>
2019-10-20 10:56:02 +0200

    Made config parameters optional

--- src/models/config.model.ts
+++ src/models/config.model.ts
@@ -2,7 +2,7 @@ import { Server } from 'ws';
 import { IPersistanceStrategy } from './persistance-strategy.model';
 export interface IConfig {
   websocket: Server;
-  fileLocation: string;
-  persistenceStrategy: IPersistanceStrategy;
-  redundancyLimit: number;
+  fileLocation?: string;
+  persistenceStrategy?: IPersistanceStrategy;
+  redundancyLimit?: number;
 }
