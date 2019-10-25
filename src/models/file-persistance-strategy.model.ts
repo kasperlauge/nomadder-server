b28a3260c3c6b0461174f9e16e13fabc16c917a7
kasperlauge <kasperlaugemadsen@gmail.com>
2019-10-25 08:53:23 +0200

    fixed type error

--- src/models/file-persistance-strategy.model.ts
+++ src/models/file-persistance-strategy.model.ts
@@ -13,11 +13,6 @@ export class FilePersistanceStrategy implements IPersistanceStrategy {
   }
 
   public persistData(db: ILocalData) {
-    const serverDataFile = `${this.fileLocation}/server-data.json`;
-    fs.writeFile(serverDataFile, JSON.stringify(db.serverDataInfo), () => {
-      // tslint:disable-next-line: no-console
-      console.log('Cached data');
-    });
     const collectionLocation = `${this.fileLocation}/collections`;
     if (fs.existsSync(collectionLocation)) {
       db.groupedServerData.forEach(gds => {
@@ -39,14 +34,8 @@ export class FilePersistanceStrategy implements IPersistanceStrategy {
   }
 
   public retrieveCache(): ILocalData {
-    const localData = { serverDataInfo: [], groupedServerData: [] } as ILocalData;
-    const serverDataPath = `${this.fileLocation}/server-data.json`;
+    const localData = { groupedServerData: [] } as ILocalData;
     const collectionPath = `${this.fileLocation}/collections`;
-    if (fs.existsSync(serverDataPath)) {
-      const serverData = fs.readFileSync(`${this.fileLocation}/server-data.json`, 'utf-8');
-      const serverDataInfo = JSON.parse(serverData);
-      localData.serverDataInfo = serverDataInfo;
-    }
     if (fs.existsSync(collectionPath)) {
       const fileNames = fs.readdirSync(collectionPath, 'utf-8');
       fileNames.forEach(fileName => {
